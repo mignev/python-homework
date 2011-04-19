9e9a0f58d26878ca63816cbc2cedbf856e2abb54
Marian Ignev <m.ignev@gmail.com>
2011-04-20 00:36:15 +0300

    The solution of my first python homework

--- 2011/01/solution.py
+++ 2011/01/solution.py
@@ -1,14 +1,14 @@
-def make_multiset(objects):
-    result = {}
-    for obj in objects:
-        result[obj] = result.get(obj, 0) + 1
+def make_multiset( _list ):
+    result = dict()
+    for item in _list:
+        result[item] = 1 if item not in result.keys() else result[item]+1 
     return result
 
-def ordered_dict(dictionary):
-    return sorted(dictionary.items())
+def ordered_dict( _dict ):
+    return [(key, _dict[key]) for key in _dict.keys()]
 
-def reversed_dict(dictionary):
-    return dict(map(reversed, dictionary.items()))
+def reversed_dict( _dict ):
+    return { value: key for key, value in zip( _dict.keys(), _dict.values() ) } 
 
-def unique_objects(objects):
-    return len(set(map(id, objects)))
+def unique_objects( _list ):
+    return len( set( id(item) for item in _list) )
